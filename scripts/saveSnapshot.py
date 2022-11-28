2d6fcbf15c976318bb6a2ff6b9d21208031b32f3
0xSacha <90143060+0xSacha@users.noreply.github.com>
2022-11-28 13:48:32 +0100

    d

--- scripts/saveSnapshot.py
+++ scripts/saveSnapshot.py
@@ -1,16 +1,16 @@
 import click
 from ape.cli import network_option, NetworkBoundCommand
 from ape import accounts, project
+import json
 
 
 def main():
-    print("Hello world!")
     account = accounts.load("sacha")
-    contract = project.DebtAllocator.at("0x9C1b710e3D2Af3B26E27467cEeb3369807fC582d")
+    contract = project.DebtAllocator.at("0x11Bff0E8f8cAe5b5E697EABCbaa379787DfdBC83")
     tx = contract.saveSnapshot(sender=account)
     logs = list(tx.decode_logs(contract.NewSnapshot))
     print(logs)
-    # assert logs == 787
+    
 
 
 
--- scripts/verifySolution.py
+++ scripts/verifySolution.py
@@ -1,16 +1,13 @@
-import click
-from ape.cli import network_option, NetworkBoundCommand
+import click 
 from ape import accounts, project
-
 
 def main():
     print("Hello world!")
     account = accounts.load("sacha")
-    contract = project.DebtAllocator.at("0x9C1b710e3D2Af3B26E27467cEeb3369807fC582d")
+    contract = project.DebtAllocator.at("0x9C1b710e3D2Af3B26E27467cEeb3369807fC582d", sender=account)
     tx = contract.saveSnapshot(sender=account)
     logs = list(tx.decode_logs(contract.NewSnapshot))
     print(logs)
-    # assert logs == 787
 
 
 

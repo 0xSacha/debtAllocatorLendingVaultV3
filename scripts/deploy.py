aba44cf7fba24a8e0cabdc5c4df1b301b65e9ecf
0xSacha <90143060+0xSacha@users.noreply.github.com>
2022-12-01 17:11:48 +0100

    h

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,7 +5,7 @@ from ape import accounts, project
 CAIRO_VERIFIER = "0xAB43bA48c9edF4C2C4bB01237348D1D7B28ef168"
 CAIRO_PROGRAM_HASH = "0x18261fedf8bb9295db94450fdda4343f1b04d3ae08f198d079a0e178596f494"
 def main():
-    account = accounts.load("sacha")
+    account = accounts.load("sach")
     contract = project.DebtAllocator.deploy(CAIRO_VERIFIER, CAIRO_PROGRAM_HASH, sender=account)
     project.track_deployment(contract)
 

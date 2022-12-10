0dd0a4978eeebaa7ee99392be0b386dcd31509c4
jmonteer <jmonteer@protonmail.com>
2022-12-10 11:58:58 +0100

    chore: adding missing files

---
+++ cairoScripts/test/test_client.py
@@ -0,0 +1,56 @@
+from starkware.cairo.sharp.client_lib import CairoPie, ClientLib
+from starkware.cairo.sharp.fact_checker import FactChecker
+from starkware.cairo.bootloaders.generate_fact import get_program_output
+from starkware.cairo.sharp.sharp_client import SharpClient
+from starkware.cairo.lang.compiler.assembler import Program
+from typing import List, Optional
+import json
+import os
+
+
+# To Set
+BIN_DIR = '../bin'
+
+RPC= 'https://mainnet.infura.io/v3/eebdf8732cd044f0a52f976af7781260'
+CAIRO_PROGRAM_PATH = './test_sharp.cairo'
+CAIRO_PROGRAM_INPUT_PATH = './test_sharp_input.json'
+
+
+
+def init_client(bin_dir: str, node_rpc_url: Optional[str] = None) -> SharpClient:
+    """
+    Initialized a SharpClient instance, with or without node access.
+    """
+    # Load configuration file.
+    CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.json")
+    with open(CONFIG_PATH, "r") as config_file:
+        config = json.load(config_file)
+
+    # Get Cairo toolchain executable paths.
+    CAIRO_COMPILE_EXE = os.path.join(os.path.join(bin_dir, "cairo-compile"))
+    CAIRO_RUN_EXE = os.path.join(os.path.join(bin_dir, "cairo-run"))
+
+    # Initialize the SharpClient.
+    client = SharpClient(
+        contract_client=FactChecker(
+            fact_registry_address=config["verifier_address"],
+            node_rpc_url=node_rpc_url if node_rpc_url is not None else "",
+        ),
+        steps_limit=config["steps_limit"],
+        cairo_compiler_path=CAIRO_COMPILE_EXE,
+        cairo_run_path=CAIRO_RUN_EXE,
+    )
+
+    return client
+
+
+client = init_client(BIN_DIR, [RPC])
+compiled_program = client.compile_cairo(CAIRO_PROGRAM_PATH)
+cairo_pie = client.run_program(compiled_program, CAIRO_PROGRAM_INPUT_PATH)
+print(f"Program output : {get_program_output(cairo_pie)}")
+print(f"Submitting to sharp")
+fact = client.get_fact(cairo_pie)
+job_key = client.submit_cairo_pie(cairo_pie=cairo_pie)
+print(f"Job key: {job_key}")
+print(f"Fact: {fact}")
+
---
+++ cairoScripts/test/test_sharp.cairo
@@ -0,0 +1,30 @@
+%builtins output range_check bitwise 
+from starkware.cairo.common.serialize import serialize_word
+from starkware.cairo.common.cairo_builtins import BitwiseBuiltin
+
+
+func check_sum{ range_check_ptr }(_op1: felt, _op2: felt) -> (state: felt){
+    alloc_locals;
+    let sum_ = _op1 + _op2;
+    if(sum_ == 5){
+        return(1,);
+    } else {
+        return(0,);
+    }
+}
+
+
+func main{output_ptr: felt*, range_check_ptr, bitwise_ptr : BitwiseBuiltin*}() {
+    alloc_locals;
+    local operand1:felt;
+    local operand2:felt;
+    %{  
+        ids.operand1 = program_input['operand1']
+        ids.operand2 = program_input['operand2']
+    %}
+    let (result_) = check_sum(operand1, operand2);
+    serialize_word(operand1);
+    serialize_word(operand2);
+    serialize_word(result_);
+    return ();
+}

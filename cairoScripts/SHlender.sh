b887dd40eb17ca3fbaa0e3c24592a0e828a5a8ca
jmonteer <jmonteer@protonmail.com>
2022-12-10 11:58:38 +0100

    chore: restructuring

--- cairoScripts/SHlender.sh
+++ cairoScripts/SHlender.sh
@@ -1,3 +1,4 @@
 cairo-compile apy_calculator.cairo  --output=apy_calculator_compiled.json
-cairo-run --program=apy_calculator_compiled.json --program_input=apy_calculator_lender_input.json --print_output --layout=all --cairo_pie_output=apy_calculator.pie
-cairo-sharp submit --cairo_pie apy_calculator.pie
+cairo-hash-program --program=apy_calculator_compiled.json
+cairo-run --program=apy_calculator_compiled.json --program_input=input/apy_calculator_lender_input.json --print_output --layout=all --cairo_pie_output=output/apy_calculator.pie
+cairo-sharp submit --cairo_pie output/apy_calculator.pie

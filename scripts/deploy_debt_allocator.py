28b8f7a66751fbf47c81199bd7a3a8c2609ae7f7
0xSacha <90143060+0xSacha@users.noreply.github.com>
2023-01-12 15:25:28 +0100

    Update deploy_debt_allocator.py

--- scripts/deploy_debt_allocator.py
+++ scripts/deploy_debt_allocator.py
@@ -2,7 +2,10 @@ from ape import accounts, project
 import json
 import os
 
+
+
 def main():
+    VAULT_ADDRESS = ""
     CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config_testnet.json")
     with open(CONFIG_PATH, "r") as config_file:
         config = json.load(config_file)
@@ -11,6 +14,6 @@ def main():
     CAIRO_VERIFIER = config["verifier_address"]
 
     account = accounts.load(config["account"])
-    contract = project.DebtAllocator.deploy(CAIRO_VERIFIER, CAIRO_PROGRAM_HASH, sender=account, max_priority_fee="1 gwei")
+    contract = project.DebtAllocator.deploy(CAIRO_VERIFIER, CAIRO_PROGRAM_HASH, VAULT_ADDRESS, sender=account, max_priority_fee="1 gwei")
     project.track_deployment(contract)
     
